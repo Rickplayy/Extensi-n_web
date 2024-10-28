f74ddc6629157f5825093168db840aa5db6fe88f
Miguel Angel G M <78458939+MiguelGM5@users.noreply.github.com>
2024-10-28 17:56:13 -0600

    Creacion de GoVO
    Ya decifre como sirve

--- GoVO/popup.js
+++ GoVO/popup.js
@@ -1,6 +1,62 @@
-const mensaje = document.querySelector('div');
-const boton = document.querySelector('#cambiar');
+// Palabras que queremos cubrir
+const palabrasOfensivas = ["put", "pendej", "culer", "puñeter", "ramer"];
+let guardaPalabras = {};
 
-boton.addEventListener('click', () => {
-    mensaje.textContent = "nuevo mensaje";
+// Función para crear la expresión regular
+function crearExpresion(palabras) {
+    const sufijos = "(o|a|os|as|itos|ito|itas)?";  // Sufijos opcionales
+    const patron = palabras.map(palabra => `${palabra}${sufijos}`).join('|');
+    return new RegExp(`\\b(${patron})\\b`, 'gi');  // Expresión regular con delimitadores de palabra
+}
+
+// Creamos la expresion regular 
+const expresionRegular = crearExpresion(palabrasOfensivas);
+console.log("EXPRESIÓN regular: " + expresionRegular);
+
+// Función para censurar el texto usando la expresión regular
+function censurarTexto(texto) {
+    if(!texto) return'';
+    return texto.replace(expresionRegular, (palabraOriginal)=> {
+        let censura = "***";
+        let palabraNormal = palabraOriginal.toLowerCase();
+        if(!guardaPalabras [palabraNormal]){
+            guardaPalabras[palabraNormal] = palabraOriginal;
+        }
+        return censura;
+        
+    });  // Esto eemplaza las palabras censuradas con '***'
+}
+//Funcion para descensurar el texto 
+function DescensurarTexto(texto){
+    if(!texto) return'';
+    for(let palabraOriginal in guardaPalabras){
+        let censura = guardaPalabras[palabraOriginal];
+        let inversa = new RegExp(`\\*\\*\\*`, 'g');
+        texto = texto.replace(inversa, guardaPalabras[palabraOriginal]);
+    }
+    return texto;
+}
+
+// Función para recorrer toda la pagina alv
+function censurarPagina() {
+    // Seleccionar todos los elementos del cuerpo de la página
+    const elements = document.body.querySelectorAll('*');
+
+    elements.forEach(element => {
+        // Evitar reemplazar en inputs, botones y otros elementos que no sean de texto esto lo puse porque me bota error si lo quito
+        if (element.children.length === 0 && element.textContent) {
+            element.textContent = censurarTexto(element.textContent);
+        }
+    });
+}
+
+const boton = document.querySelector('#btn-Censura');
+
+boton.addEventListener('click', () =>{
+    alert("si entra aca");
+    console.log("aca entra tambien");
+    return censurarPagina();
 });
+
+
+
