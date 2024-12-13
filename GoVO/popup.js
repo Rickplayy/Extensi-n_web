6d5aa4f6df6986c88ca27b83bfd764e6f070ba9b
Ian Rivero <iianriverk@gmail.com>
2024-12-13 02:02:18 -0600

    Salvando cambios para regex
    Salvando cambios para regex

--- GoVO/popup.js
+++ GoVO/popup.js
@@ -147,18 +147,26 @@ botonEliminar.addEventListener("click", () => {
 // Esta función se ejecutará en el contexto de la página web
 function censurarTextoEnPagina() {
     //variables provisonales en lo que se extraen las palabras del .md
-    //const palabrasProhibidas = ["put", "pendej", "culer", "puñeter", "ramer","jodeput", "pelotud","verg"];
-    //const sufijos = "(o|a|os|as|itos|ito|itas)?"; 
+    const palabrasProhibidas = ["put", "pendej", "culer", "puñeter", "ramer","jodeput", "pelotud","verg"];
+    const sufijos = "(o|a|os|as|itos|ito|itas)?"; 
+
+    /*
+    const regex2 = new RegExp(
+        "\\b(cabr(ón|ona|ones|oncillo)|pendej(a|os|ear|itos|as|etes|eo|ita|adas)|mierd(a|itas|ero|ón|eros|as)|coñ(o|os|azo|azos)|chinga(da|ndo|r|das|dera)|cul(eros|eras|culerito|ear|ero|era)|desgraciad(o|a|os|as)|mam(ona|oncillos|ones|onas)|hijueput(as|ón)|malparid(os|a|as)|idiot(a|as|ita|ez)|pinch(es|e)|puñet(ero|a|eros|eras)|ojet(es|e)|perr(a|as)|mamahuevo(s)?)\\b",
+        "gi"
+      );
+    */
+
     const reemplazo = "***";
 
     function censurarNodo(node) {
         if (node.nodeType === Node.TEXT_NODE) {
-            let texto = node.nodeValue;
+            let textoAltisonantes = node.nodeValue;
             palabrasProhibidas.forEach((palabra) => {
-                const regex = new RegExp(`\\b${palabra}${sufijos}\\b`, 'gi');
-                texto = texto.replace(regex, reemplazo);
+                const regex = new RegExp("\\b(cabr(ón|ona|ones|oncillo)|pendej(a|os|ear|itos|as|etes|eo|ita|adas)|mierd(a|itas|ero|ón|eros|as)|coñ(o|os|azo|azos)|chinga(da|ndo|r|das|dera)|cul(eros|eras|culerito|ear|ero|era)|desgraciad(o|a|os|as)|mam(ona|oncillos|ones|onas)|hijueput(as|ón)|malparid(os|a|as)|idiot(a|as|ita|ez)|pinch(es|e)|puñet(ero|a|eros|eras)|ojet(es|e)|perr(a|as)|mamahuevo(s)?)\\b","gi");
+                textoAltisonantes = textoAltisonantes.replace(regex, reemplazo);
             });
-            node.nodeValue = texto;
+            node.nodeValue = textoAltisonantes;
         } else if (node.nodeType === Node.ELEMENT_NODE) {
             node.childNodes.forEach(censurarNodo);
         }
@@ -168,8 +176,8 @@ function censurarTextoEnPagina() {
 
 function borrarTextoEnPagina() {
     //variables provisonales en lo que se extraen las palabras del .md
-    //const palabrasProhibidas = ["put", "pendej", "culer", "puñeter", "ramer","jodeput", "pelotud","verg"];
-    //const sufijos = "(o|a|os|as|itos|ito|itas)?"; 
+    const palabrasProhibidas = ["put", "pendej", "culer", "puñeter", "ramer","jodeput", "pelotud","verg"];
+    const sufijos = "(o|a|os|as|itos|ito|itas)?"; 
     const borrado = "";
 
   
@@ -177,7 +185,7 @@ function borrarTextoEnPagina() {
         if (node.nodeType === Node.TEXT_NODE) {
             let texto2 = node.nodeValue;
             palabrasProhibidas.forEach((palabra2) => {
-                const regex = new RegExp(`\\b${palabra2}${sufijos}\\b`, 'gi');
+                const regex = new RegExp("\\b(cabr(ón|ona|ones|oncillo)|pendej(a|os|ear|itos|as|etes|eo|ita|adas)|mierd(a|itas|ero|ón|eros|as)|coñ(o|os|azo|azos)|chinga(da|ndo|r|das|dera)|cul(eros|eras|culerito|ear|ero|era)|desgraciad(o|a|os|as)|mam(ona|oncillos|ones|onas)|hijueput(as|ón)|malparid(os|a|as)|idiot(a|as|ita|ez)|pinch(es|e)|puñet(ero|a|eros|eras)|ojet(es|e)|perr(a|as)|mamahuevo(s)?)\\b","gi");
                 texto2 = texto2.replace(regex, borrado);
             });
             node.nodeValue = texto2;
